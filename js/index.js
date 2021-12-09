60a88db01eb7d02cf544ff37f6bb0cb4d8962fb9
LINDSEY-KIM <88068327+LINDSEY-KIM@users.noreply.github.com>
2021-12-09 22:48:37 +0900

    site renewal bluebottle

---
+++ js/index.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+
+        // var sct = $(window).scrollTop();
+        // // console.log(sct);
+
+        // var sct_con_1 = $(".section_1").offset().top; 
+        // var sct_con_2 = $(".section_2").offset().top; 
+        // var sct_con_3 = $(".section_3").offset().top; 
+        // var sct_con_4 = $(".section_4").offset().top; 
+        // var sct_con_5 = $(".section_5").offset().top; 
+        // var sct_con_6 = $(".section_6").offset().top;
+        // var sct_con_7 = $(".section_7").offset().top;
+
+
+        // var sct_img_2 = $('.img_wrap_2').offset().top;
+        // // console.log(sct_img_2)
+
+
+        // if(sct >= -1){
+        //     $('.txt_wrap_1 h1').addClass('active_2');
+        // }
+
+        // if(sct >= 3200){
+        //     $('.txt_wrap_3 h2').addClass('active_3');
+        // }
+
+        // if(sct >= 3400){
+        //     $('.txt_wrap_3 h1').addClass('active_4');
+        // }
+
+
+        // // pop motion end
+
+
+        // if(sct >= sct_img_2 * 0.5){
+        //     $('.right_img_box .move_1').css({
+        //         display: 'block',
+        //         opacity: '1'
+        //     });
+
+        //     $('.right_img_box .move_2').css({
+        //         display: 'none'
+        //     });
+            
+        //     $('.right_img_box .move_3').css({
+        //         display: 'none'
+        //     });
+
+        // }
+
+        // if(sct >= sct_img_2 * 1.2){
+        //     $('.right_img_box .move_1').css({
+        //         display: 'none'
+        //     });
+
+        //     $('.right_img_box .move_2').css({
+        //         display: 'block'
+        //     });
+
+        //     $('.right_img_box .move_3').css({
+        //         display: 'none'
+        //     });
+
+        // }
+
+        // if(sct >= sct_img_2 * 1.7){
+        //     $('.right_img_box .move_1').css({
+        //         display: 'none'
+        //     });
+
+        //     $('.right_img_box .move_2').css({
+        //         display: 'none'
+        //     });
+
+        //     $('.right_img_box .move_3').css({
+        //         display: 'block'
+        //     });
+        // }
+
+
+
+        // if(sct >= sct_img_2 * 2.1){
+        //     $('.right_img_box .move_1').css({
+        //         display: 'none'
+        //     });
+
+        //     $('.right_img_box .move_2').css({
+        //         display: 'none'
+        //     });
+
+        //     $('.right_img_box .move_3').css({
+        //         display: 'none',
+        //         // opacity: '0',
+        //     });
+        // }
+
+    
+        // // section_2 move motion end
+
+
+
+
+        // var useScroll = sct / sct_con_4 ;
+
+
+        // if (sct >= sct_con_3  && sct < sct_con_4 ) {
+        //     console.log(useScroll);
+
+        //     $(".title_wrap_4").css({
+        //         opacity: useScroll,
+        //     });
+
+        // }
+
+
+    });
+
+
+
+
+});
